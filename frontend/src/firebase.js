b66cbd75cd7cf2aebc0bd2c7744b8e9512dfe4ee
Shruti <94033084+shruti0503@users.noreply.github.com>
2023-09-14 08:11:30 +0530

    Update firebase.js
    ap hidden

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBBNR0HUhUf4WNh1p7vJ6sua9qyFqhCOXk",
+  apiKey: "A8*******************",
   authDomain: "calendlio.firebaseapp.com",
   projectId: "calendlio",
   storageBucket: "calendlio.appspot.com",
-  messagingSenderId: "1004749557336",
-  appId: "1:1004749557336:web:7c67ca0791fe94ba9cc42d",
-  measurementId: "G-VSW0NQ175G"
+  messagingSenderId: "**************",
+  appId: "*********************",
+  measurementId: "******************"
 };
 
 // Initialize Firebase
